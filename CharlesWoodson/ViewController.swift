166f264faec5127665cf63508e9156dba6cf0cb7
Omar Al-Hayderi <oahayder@gmail.com>
2015-11-16 18:12:34 -0800

    Initial Commit

---
+++ CharlesWoodson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CharlesWoodson
+//
+//  Created by Omar Al-Hayderi on 11/16/15.
+//  Copyright © 2015 testy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
